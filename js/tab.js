853ee5a62de5eb7f6412e8019b4b12f2176270e1
Anton Smolko <asmolko@yandex.ru>
2018-04-22 15:33:46 +0300

    Сохранен tab.js

--- js/tab.js
+++ js/tab.js
@@ -5,6 +5,8 @@ export default class Tab {
      */
     constructor(element, onActivate = false) {
         this.element = element;
+        this.onActivate = inActivate;
+        this.init();
     }
 
     /**
